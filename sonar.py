bd08fac6855d1954aefa4dbeda6e1b750a523fc3
jez <jez.wain@atos.net>
2023-12-26 10:00:17 +0000

    Initial commit

---
+++ sonar.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+bmc_name_pattern = 'pise*-bmc'
+redfish_root = '/redfish/v1'
+
+def bmc_iterator():
+    min_bmc_num = 1
+    max_bmc_num = 500
+
+    for bmc_num in range(min_bmc_num, max_bmc_num + 1):
+        yield bmc_name_pattern.replace('*', f'{bmc_num:03}')
+
+
+def read_credentials(credentials_file):
+    with open(credentials_file) as f:
+        return [tuple(user_pass.split()) for user_pass in f.readlines()]
+
+bmcs = {}
+
+for bmc in bmc_iterator():
+    bmc_info = bmcs.get(bmc, {})
+
+    # Determine if there's a redfish server on bmc
+    url = 'https://' + bmc + redfish_root
+    resp = requests.get(url)
+    has_redfish = resp.status_code == requests.codes.ok
+    bmc_info['redfish'] = has_redfish
+
+    if has_redfish and bmc_info.get('user_pass') is None:
+        for user_pass in read_credentials('credentials.txt'):
+            chassis_url = url + '/Chassis'
+            resp = requests.get(chassis_url, auth=user_pass)
+            if resp.status_code == requests.codes.ok:
+                bmc_info['user_pass'] = user_pass
+                print(json.dumps(resp.json(), indent=3, sort_keys=True))
+                bmcs[bmc] = bmc_info
+                break
+            print(f"HTTP Status: {resp.status_code}")
+
+
+print(f"{'BMC':>10} Has redfish Username/password")
+print('=' * 38)
+for bmc, info in bmcs.items():
+    print(f"{bmc:>10} {info.get('redfish'):11} {"/".join(info.get('user_pass', ['** UNKNOWN **']))}")
+
+with open('bmc_info.json', 'w') as f:
+    json.dump(bmcs, f, indent=3, sort_keys=True)
+
